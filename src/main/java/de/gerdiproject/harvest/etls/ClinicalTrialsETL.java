d091345ecbf52a65fef9564389695161179c6c64
row@informatik.uni-kiel.de <Robin Wei>
2019-02-28 14:23:09 +0100

    Bamboo: Created harvester repository for the provider 'ClinicalTrials'.

---
+++ src/main/java/de/gerdiproject/harvest/etls/ClinicalTrialsETL.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright © 2019 Komal Ahir (http://www.gerdi-project.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.gerdiproject.harvest.etls;
+
+import de.gerdiproject.harvest.etls.extractors.ClinicalTrialsExtractor;
+import de.gerdiproject.harvest.etls.extractors.ClinicalTrialsVO;
+import de.gerdiproject.harvest.etls.transformers.ClinicalTrialsTransformer;
+import de.gerdiproject.json.datacite.DataCiteJson;
+
+
+/**
+ * An ETL for harvesting ClinicalTrials.<br>
+ * See: https://clinicaltrials.gov/ct2/resources/download
+ *
+ * @author Komal Ahir
+ */
+public class ClinicalTrialsETL extends StaticIteratorETL<ClinicalTrialsVO, DataCiteJson>
+{
+    /**
+     * Constructor
+     */
+    public ClinicalTrialsETL()
+    {
+        super(new ClinicalTrialsExtractor(), new ClinicalTrialsTransformer());
+    }
+
+    // TODO 1. Check if StaticIteratorETL really suits your needs, or exchange it with any other AbstractETL.
+    // TODO 2. Exchange ClinicalTrialsVO with whatever is extracted from your DataProvider or populate it with fitting data.
+    // TODO 3. Override registerParameters() if you need to register additional ETL parameters.
+    // TODO 4. Override any other methods if needed.
+}
